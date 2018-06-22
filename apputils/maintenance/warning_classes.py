2d4ec89c270d57715b2907e2d326cc9cc2e0b17a
Andy Babic <ababic@rkh.co.uk>
2018-06-22 19:41:42 +0100

    Rename apputils.utils.deprecation to apputils.maintenance.warning_classes

--- apputils/maintenance/warning_classes.py
+++ apputils/maintenance/warning_classes.py
@@ -8,6 +8,8 @@ removed_in_next_version_warning = RemovedInDjangoAppUtils02Warning
 class RemovedInDjangoAppUtils03Warning(PendingDeprecationWarning):
     pass
 
+removed_in_following_version_warning = RemovedInDjangoAppUtils03Warning
+
 
 class RemovedInDjangoAppUtils04Warning(PendingDeprecationWarning):
     pass
