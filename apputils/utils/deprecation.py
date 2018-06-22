4f3afbf860bf184272df13596cf80b1b9c10c4e4
Andy Babic <ababic@rkh.co.uk>
2018-06-22 11:06:20 +0100

    Added some project-specific deprecation warnings that will be cycled with each minor release to help with deprecation

---
+++ apputils/utils/deprecation.py
@@ -0,0 +1,13 @@
+class RemovedInDjangoAppUtils02Warning(DeprecationWarning):
+    pass
+
+
+removed_in_next_version_warning = RemovedInDjangoAppUtils02Warning
+
+
+class RemovedInDjangoAppUtils03Warning(PendingDeprecationWarning):
+    pass
+
+
+class RemovedInDjangoAppUtils04Warning(PendingDeprecationWarning):
+    pass
