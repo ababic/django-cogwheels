680f9e7e96d6da09a65382c623a69926c12333d6
Andy Babic <ababic@rkh.co.uk>
2018-06-22 11:07:41 +0100

    Added some more default app settings to use in testing

--- apputils/tests/conf/defaults.py
+++ apputils/tests/conf/defaults.py
@@ -1,3 +1,7 @@
+# -----------------------------------------------------------------------------
+# Standard type settings
+# -----------------------------------------------------------------------------
+
 INTEGER_SETTING = 1
 
 BOOLEAN_SETTING = False
@@ -11,8 +15,28 @@ TUPLES_SETTING = (
     (4, 'Four'),
 )
 
+
+# -----------------------------------------------------------------------------
+# Model/class and module settings
+# -----------------------------------------------------------------------------
+
 MODEL_SETTING = 'tests.DefaultModel'
 
 CLASS_SETTING = 'tests.classes.DefaultClass'
 
 MODULE_SETTING = 'tests.modules.default_module'
+
+
+# -----------------------------------------------------------------------------
+# Deprecations
+# -----------------------------------------------------------------------------
+
+DEPRECATED_SETTING = 'i-am-deprecated'
+
+REPLACED_SETTING = 'i-have-been-renamed'
+
+REPLACEMENT_SETTING = 'i-am-a-direct-replacement-of-another-setting'
+
+SUPERSEDED_SETTING = 'i-have-been-superseded'
+
+SUCCESSOR_SETTING = 'i-have-succeeded-another-setting'
