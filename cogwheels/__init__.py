2be0c5bfd3466466946a2f1a40410164a2b30cb7
Andy Babic <ababic@rkh.co.uk>
2018-06-25 15:55:20 +0100

    Fix imports in __init__

--- cogwheels/__init__.py
+++ cogwheels/__init__.py
@@ -1,11 +1,11 @@
+from .__version__ import ( # noqa
+    __title__, __description__, __version__,
+    __author__, __author_email__,
+    __copyright__, __license__
+)
 from .exceptions import ( # noqa
     InvalidSettingValueType, InvalidSettingValueFormat,
     InvalidDefaultValueType, InvalidDefaultValueFormat,
     SettingValueNotImportable, DefaultValueNotImportable,
 )
 from .helpers import BaseAppSettingsHelper, DeprecatedAppSetting # noqa
-from .version import ( # noqa
-    __title__, __description__, __version__,
-    __author__, __author_email__,
-    __copyright__, __license__
-)
