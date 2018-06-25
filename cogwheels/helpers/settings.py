bffe2f09a77b8811e44d779af4e9a5bfabb2b6f2
Andy Babic <ababic@rkh.co.uk>
2018-06-25 18:06:44 +0100

    Rename HelperethodAttrWrapper to AttriToMethodCallHelper, and move into a cogwheels.helpers.utils

--- cogwheels/helpers/settings.py
+++ cogwheels/helpers/settings.py
@@ -11,48 +11,7 @@ from cogwheels.exceptions.deprecations import (
     DuplicateDeprecationError, DuplicateDeprecationReplacementError,
 )
 
-
-class HelperMethodAttrWrapper:
-    """
-    ``BaseAppSettingsHelper`` creates several instances of this class on
-    initialisation to allow developers to neatly reference settings to get a
-    value cast as a certain type of object. Behind the scenes, attribute
-    requests on ``HelperMethodAttrWrapper`` instance are forwarded on to
-    one of the helper instance's 'get_x()' methods.
-
-    For example, if you want the actual python module referenced by a setting,
-    instead of doing this:
-
-    ``appsettingshelper.get_module('MODULE_SETTING_NAME')``
-
-    The 'modules' ``HelperMethodAttrWrapper`` instance (set as an attribute
-    on every setting helper) allows you to do this:
-
-    ``appsettingshelper.modules.MODULE_SETTING_NAME``
-
-    An 'objects' attribute also allows you to neatly access python objects from
-    setting values too, like:
-
-    ``appsettingshelper.objects.OBJECT_SETTING_NAME``
-
-    And 'models' allows you to access Django models, like:
-
-    ``appsettingshelper.models.MODEL_SETTING_NAME``
-
-    """
-    def __init__(self, settings_helper, getter_method_name):
-        self.settings_helper = settings_helper
-        self.getter_method_name = getter_method_name
-
-    def __getattr__(self, name):
-        if self.settings_helper.in_defaults(name):
-            return self.get_value_via_helper_method(name)
-        raise AttributeError("{} object has no attribute '{}'".format(
-            self.settings_helper.__class__.__name__, name))
-
-    def get_value_via_helper_method(self, setting_name):
-        method = getattr(self.settings_helper, self.getter_method_name)
-        return method(setting_name)
+from .utils import AttrToMethodCallHelper
 
 
 class BaseAppSettingsHelper:
@@ -74,9 +33,9 @@ class BaseAppSettingsHelper:
         self._django_settings = settings
         self._import_cache = {}
         self.perepare_deprecation_data()
-        self.modules = HelperMethodAttrWrapper(self, 'get_module')
-        self.objects = HelperMethodAttrWrapper(self, 'get_object')
-        self.models = HelperMethodAttrWrapper(self, 'get_model')
+        self.modules = AttrToMethodCallHelper(self, 'get_module')
+        self.objects = AttrToMethodCallHelper(self, 'get_object')
+        self.models = AttrToMethodCallHelper(self, 'get_model')
         setting_changed.connect(self.clear_caches, dispatch_uid=id(self))
 
     @property
