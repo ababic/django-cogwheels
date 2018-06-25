da9571c7fb55acbac76929c17cad0551f7833aa4
Andy Babic <ababic@rkh.co.uk>
2018-06-25 13:20:18 +0100

    Split invalid deprecation value tests out into their own module and test that the correct exception are used, rather than testing the message text

--- cogwheels/helpers/tests/test_helper_init.py
+++ cogwheels/helpers/tests/test_helper_init.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ImproperlyConfigured
 from django.test import TestCase
 
 from cogwheels.helpers import BaseAppSettingsHelper, DeprecatedAppSetting
@@ -35,43 +34,6 @@ class TestHelperInit(TestCase):
             test_val
         )
 
-
-class TestHelperInitErrors(TestCase):
-
-    def test_errors_if_defaults_path_is_invalid(self):
+    def test_raises_import_error_if_defaults_module_cannot_be_imported(self):
         with self.assertRaises(ImportError):
             TestSettingsHelper(defaults_path='invalid.module.path')
-
-    def test_errors_if_deprecations_not_list_or_tuple(self):
-        message_expected = (
-            "'deprecations' must be a list or tuple, not a dict."
-        )
-        with self.assertRaisesRegex(ImproperlyConfigured, message_expected):
-            TestSettingsHelper(deprecations={})
-
-    def test_errors_no_default_value_found_for_a_deprecatedappsetting(self):
-        message_expected = (
-            "There is an issue with one of your setting deprecation "
-            "definitions. 'NON_EXISTENT_SETTING' could not be found in "
-            "cogwheels.tests.conf.defaults. Please ensure a default "
-            "value remains there until the end of the setting's deprecation "
-            "period."
-        )
-        with self.assertRaisesRegex(ImproperlyConfigured, message_expected):
-            TestSettingsHelper(deprecations=(
-                DeprecatedAppSetting('NON_EXISTENT_SETTING'),
-            ))
-
-    def test_errors_if_invalid_replacement_setting_value_used_for_a_deprecatedappsetting(self):
-        message_expected = (
-            "There is an issue with one of your settings deprecation "
-            "definitions. 'NON_EXISTENT_SETTING' is not a valid replacement "
-            "for 'DEPRECATED_SETTING', as no such value can be found in "
-            "cogwheels.tests.conf.defaults."
-        )
-        with self.assertRaisesRegex(ImproperlyConfigured, message_expected):
-            TestSettingsHelper(deprecations=(
-                DeprecatedAppSetting(
-                    'DEPRECATED_SETTING', renamed_to="NON_EXISTENT_SETTING"
-                ),
-            ))
